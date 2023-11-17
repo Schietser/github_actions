a26d7277b671d35c11c776d26658277352f4dd31
Schietser <ruben.schietse@gmail.com>
2023-11-17 10:50:32 +0100

    fixed failed test + changed yaml

--- src/test/java/com/example/ExampleResourceTest.java
+++ src/test/java/com/example/ExampleResourceTest.java
@@ -23,7 +23,7 @@ public class ExampleResourceTest {
         RestAssured.given()
                 .when().get("/hello")
                 .then()
-                .statusCode(404);
+                .statusCode(200);
     }
 
 }
