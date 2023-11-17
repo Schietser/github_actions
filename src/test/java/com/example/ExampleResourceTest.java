865ba3b5227132ed6d92582b9dbdc2e05a5bb5c1
Schietser <ruben.schietse@gmail.com>
2023-11-17 10:31:33 +0100

    second commit

--- src/test/java/com/example/ExampleResourceTest.java
+++ src/test/java/com/example/ExampleResourceTest.java
@@ -1,21 +1,21 @@
 package com.example;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
 public class ExampleResourceTest {
 
     @Test
     public void testHelloEndpoint() {
-        given()
+        RestAssured.given()
                 .when().get("/hello")
                 .then()
                 .statusCode(200)
-                .body(is("Hello from RESTEasy Reactive"));
+                .body(equalTo("Hello from RESTEasy Reactive"));
     }
 
 }
